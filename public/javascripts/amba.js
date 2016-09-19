a016796e0a2e76d75f0e26a18c243e094e3725e5
wonyong <cwell.jeong@gmail.com>
2016-09-19 22:16:35 +0900

    add amba.js

---
+++ public/javascripts/amba.js
@@ -0,0 +1,184 @@
+function div() {
+	return new Div();
+}
+
+function Div() {
+	this.$ = $('<div></div>');
+	this.displayInlineBlock();
+	this.textSize(0);
+	this.isAddedText = false;
+	// this.align('center');
+}
+
+Div.prototype.css = function(key, value) {
+	if(value === undefined) {
+		return this.$.css(key);
+	}
+
+	this.$.css(key, value);
+	return this;
+}
+
+
+Div.prototype.attr = function(key, value) {
+	if(value === undefined) {
+		return this.$.attr(key);
+	}
+
+	this.$.attr(key, value);
+	return this;
+}
+
+Div.prototype.append = function() {
+	$('body').append(this.$);
+	return this;
+}
+
+Div.prototype.appendTo = function(parent) {
+	this.$.appendTo(parent.$);
+	return this;
+}
+
+Div.prototype.displayInlineBlock = function() {
+	this.css('display', 'inline-block');
+	return this;
+}
+
+Div.prototype.align = function(value) {
+	this.css('text-align', value);
+	return this;
+}
+
+Div.prototype.textSize = function(px) {
+	this.css('font-size', px);
+	this.isAddedText = true;
+	return this;
+}
+
+Div.prototype.text = function(txt) {
+	// this.css('font-size', px);
+	this.$.text(txt);
+	if(this.isAddedText === false) {
+		this.textSize(14);
+	}
+	return this;
+}
+
+Div.prototype.border = function(px) {
+	this.css('border', px+'px solid');
+	return this;
+}
+
+Div.prototype.borderColor = function(c) {
+	this.css('border-color', c);
+	return this;
+}
+
+Div.prototype.borderRadius = function(px) {
+	this.css('border-radius', px);
+	return this;
+}
+
+Div.prototype.color = function(c) {
+	this.css('background-color', c);
+	return this;
+}
+
+Div.prototype.width = function(px) {
+	if(px === undefined)
+		return this.css('width');
+	this.css('width', px);
+	return this;
+}
+
+Div.prototype.minWidth = function(px) {
+	if(px === undefined)
+		return this.css('min-width');
+	this.css('min-width', px);
+	return this;
+}
+
+Div.prototype.height = function(px) {
+	if(px === undefined)
+		return this.css('height');
+	this.css('height', px);
+	return this;
+}
+
+Div.prototype.minHeight = function(px) {
+	if(px === undefined)
+		return this.css('min-height');
+	this.css('min-height', px);
+	return this;
+}
+
+Div.prototype.margin = function(px) {
+	if(px === undefined)
+		return this.css('margin');
+	this.css('margin', px);
+	return this;
+}
+
+Div.prototype.padding = function(px) {
+	if(px === undefined)
+		return this.css('padding');
+	this.css('padding', px);
+	return this;
+}
+
+Div.prototype.overflow = function(value) {
+	if(value === undefined)
+		return this.css('overflow');
+	this.css('overflow', value);
+	return this;
+}
+
+Div.prototype.size = function(w, h) {
+	if(arguments.length == 0) {
+		return {
+			width: this.width(),
+			height: this.height()
+		}
+	}
+
+	this.width(w);
+	this.height(h);
+	return this;
+}
+
+Div.prototype.offset = function(x, y) {
+	if(arguments.length == 0) {
+		return this.$.offset();
+	}
+
+	this.$.offset({
+		left: x,
+		top: y
+	});
+
+	return this;
+}
+
+Div.prototype.click = function(fn) {
+	var that = this;
+	this.$.click(function(){
+		if(fn) fn(that);
+	});
+	return this;
+}
+
+Div.prototype.draggable = function(opt) {
+	this.$.draggable(opt);
+	return this;
+}
+
+Div.prototype.moveDown = function(y, delay) {
+	this.$.animate({
+		top: '+20'
+	}, delay);
+	return this;
+}
+
+function random (max) {
+	return parseInt(Math.random(max)*max);
+}
