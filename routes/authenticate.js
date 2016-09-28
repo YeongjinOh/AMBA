9ce8c22d88d55fceb4ce72e22e65bcbe803731d0
JiSoo <yjsgoon@naver.com>
2016-09-29 00:00:48 +0900

    merge

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -12,9 +12,6 @@ pgp.pg.defaults.poolSize = 10;
 pgp.pg.defaults.ssl = true;
 var db = pgp('postgres://eynernjlrihgzw:2cSSchyFA0jNj0etElZ574Y0z2@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d1o7q84tkif5t0');
 
-
-// var pg = require('pg');
-
 var token = {
     username: undefined,
     password: undefined,
