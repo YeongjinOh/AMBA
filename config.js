02b58e07145762d4cc26cbf963afccfcf2763a68
JiSoo <yjsgoon@naver.com>
2016-09-29 18:42:43 +0900

    design refactoring of sign-in, up

--- config.js
+++ config.js
@@ -4,5 +4,6 @@
 
 // HS256의 key
 module.exports = {
+    DATABASE_URL: 'postgres://eynernjlrihgzw:2cSSchyFA0jNj0etElZ574Y0z2@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d1o7q84tkif5t0',
     secret: 'ambasecret'
 };
--- db.js
+++ db.js
@@ -2,9 +2,10 @@
  * Created by JiSoo on 2016-09-28.
  */
 
+var config = require('./config');
 var pgp = require('pg-promise')();
-pgp.pg.defaults.poolSize = 10;
+pgp.pg.defaults.poolSize = 20;
 pgp.pg.defaults.ssl = true;
-var DB = pgp('postgres://eynernjlrihgzw:2cSSchyFA0jNj0etElZ574Y0z2@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d1o7q84tkif5t0');
+var DB = pgp(config.DATABASE_URL);
 
 module.exports = DB;
