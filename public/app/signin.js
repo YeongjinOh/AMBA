724e8f3aa855d5a67b6376dca1ddaa369e69706f
JiSoo <yjsgoon@naver.com>
2016-10-02 03:20:39 +0900

    re-modify before commit

--- public/app/signin.js
+++ public/app/signin.js
@@ -4,15 +4,20 @@
 
 var root = div().append().size('100%', '100%').color('#4c00e6');
 
-var form = div().appendTo(root).displayBlock().size('300px', '340px').position('relative').margin('auto').top('50%').transform('translate(0%, -50%)').color('#4c00e6');
-div().appendTo(form).displayBlock().size('240px', '60px').margin('auto').color('#4c00e6').text('AMBA').fontSize('50px').fontBold()
-     .fontColor('white').textAlignCenter().textDragNone().cursorDefault();
+var form = div().appendTo(root).displayBlock().size('300px', '340px').position('relative').margin('auto').top('50%')
+    .transform('translate(0%, -50%)').color('#4c00e6');
+div().appendTo(form).displayBlock().size('240px', '60px').margin('auto').color('#4c00e6').text('AMBA').fontSize('50px')
+    .fontBold().fontColor('white').textAlignCenter().textDragNone().cursorDefault();
 
-div().appendTo(form).displayBlock().size('270px', '20px').margin('auto').marginTop('20px').color('#4c00e6').text('Email').fontSize('15px').fontColor('white').textDragNone().cursorDefault();
-var email = div().appendTo(form).displayBlock().size('270px', '30px').margin('auto').marginTop('5px').color('white').borderRadius(5).boxShadow('5px 5px 5px black').text('').editable().fontSize('20px').cursorText();
+div().appendTo(form).displayBlock().size('270px', '20px').margin('auto').marginTop('20px').color('#4c00e6').text('Email')
+    .fontSize('15px').fontColor('white').textDragNone().cursorDefault();
+var email = div().appendTo(form).displayBlock().size('270px', '30px').margin('auto').marginTop('5px').color('white')
+    .borderRadius(5).boxShadow('5px 5px 5px black').text('').editable().fontSize('20px').cursorText();
 
-div().appendTo(form).displayBlock().size('270px', '20px').margin('auto').marginTop('15px').color('#4c00e6').text('Password').fontSize('15px').fontColor('white').textDragNone().cursorDefault();
-var password = div().appendTo(form).displayBlock().size('270px', '30px').margin('auto').marginTop('5px').color('white').borderRadius(5).boxShadow('5px 5px 5px black').text('').editable().textPassword().fontSize('20px').cursorText();
+div().appendTo(form).displayBlock().size('270px', '20px').margin('auto').marginTop('15px').color('#4c00e6').text('Password')
+    .fontSize('15px').fontColor('white').textDragNone().cursorDefault();
+var password = div().appendTo(form).displayBlock().size('270px', '30px').margin('auto').marginTop('5px').color('white')
+    .borderRadius(5).boxShadow('5px 5px 5px black').text('').editable().textPassword().fontSize('20px').cursorText();
 
 div().appendTo(form).displayBlock().size('270px', '40px').margin('auto').marginTop('30px').color('#1a53ff').borderRadius(5).boxShadow('5px 5px 5px black')
     .text('Sign In').textAlignCenter().fontSize('25px').fontColor('white').fontBold().textDragNone().cursorPointer().hover(function (dv) {
@@ -32,7 +37,8 @@ div().appendTo(form).displayBlock().size('270px', '40px').margin('auto').marginT
         });
 });
 
-div().appendTo(form).displayBlock().size('50px', '20px').float('right').marginRight('15px').marginTop('10px').textAlignRight().color('#4c00e6').text('Sign Up').fontSize('13px').fontColor('white').fontBold().textDragNone().cursorPointer().hover(function (dv) {
+div().appendTo(form).displayBlock().size('50px', '20px').float('right').marginRight('15px').marginTop('10px').textAlignRight()
+    .color('#4c00e6').text('Sign Up').fontSize('13px').fontColor('white').fontBold().textDragNone().cursorPointer().hover(function (dv) {
     dv.fontColor('black');
 },  function (dv) {
     dv.fontColor('white');
--- routes/users.js
+++ routes/users.js
@@ -7,34 +7,12 @@ var crypto = require('crypto');
 var db = require('../db');
 var config = require('../config');
 
-/* 기존 사용자 정보를 얻는다. */
-// router.get('/login', function (req, res, next) {
-//     if (!req.query.token)
-//         return res.json({
-//             resultCode: -1,
-//             msg:
-//         });
-//
-//     var token = jwt.decode(req.query.token, config.secret);
-//     db.one("select * from users_tb where username_cd = $1;", [token.username])
-//         .then(function () {
-//             res.json({
-//                 resultCode: 0
-//             });
-//         })
-//         .catch(function () {
-//             res.json({
-//                 resultCode: -1,
-//                 msg: '인증에 실패했습니다.'
-//             });
-//         });
-// });
-
 /* 사용자의 새로운 토큰을 생성한다. */
 router.post('/login', function (req, res, next) {
-    var password = crypto.createHmac('sha256', config.pwSecret).update(req.body.password);
     db.one("select * from users where email = $1;", [req.body.email])
         .then(function (data) {
+            var password = crypto.createHmac('sha256', config.pwSecret).update(req.body.password).digest('base64');
+
             if (data.password === password) {
                 var token = crypto.createHmac('sha256', config.secret).update(data.uid + '.' + data.status).digest('base64');
                 res.json({
@@ -60,7 +38,8 @@ router.post('/login', function (req, res, next) {
 
 /* 새로운 사용자를 생성한다. */
 router.post('/regist', function(req, res, next) {
-    var password = crypto.createHmac('sha256', config.pwSecret).update(req.body.password);
+    var password = crypto.createHmac('sha256', config.pwSecret).update(req.body.password).digest('base64');
+
     db.none("insert into users(email, password, username, ipt_date, upt_date) values($1, $2, $3, now(), now());", [req.body.email, password, req.body.username])
         .then(function () {
             res.json({
