ec8a593845559ae7bf3084f03cc240060df3c9d7
JiSoo <yjsgoon@naver.com>
2016-10-04 20:09:18 +0900

    test datastore.js

--- routes/datastore.js
+++ routes/datastore.js
@@ -6,9 +6,15 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
+
+/*
+    JSON Array Parsing하기.
+    duplicate on 문제 해결하기
+ */
+
 router.get('/put', function(req, res, next) {
     // cid, key, value
-    db.none("insert into data_store(cid, key, value) values($1, $2, $3);",       // on duplicate key update value = $4;",
+    db.none("insert into data_store(cid, key, value) values($1, $2, $3) on duplicate key update value = $4;",
             [req.query.cid, req.query.key, req.query.value, req.query.value])
         .then(function () {
             res.json({
@@ -24,7 +30,6 @@ router.get('/put', function(req, res, next) {
 });
 
 router.get('/get', function(req, res, next) {
-    //cid, key
     db.many("select value from data_store where cid = $1 and key = $2;", [req.query.cid, req.query.key])
         .then(function (data) {
             res.json({
@@ -41,7 +46,6 @@ router.get('/get', function(req, res, next) {
 });
 
 router.get('/delete', function(req, res, next) {
-    // cid, key
     db.none("delete from data_store where cid = $1 and key = $2;", [req.query.cid, req.query.key])
         .then(function (data) {
             res.json({
@@ -57,7 +61,7 @@ router.get('/delete', function(req, res, next) {
 });
 
 router.get('/keys', function(req, res, next) {
-    // cid
+    // [{"value":"value1"}]
     db.many("select key, value from data_store where cid = $1;", [req.query.cid])
         .then(function (data) {
             res.json({
