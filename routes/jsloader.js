909b48d00273d2a72f1d18fb7382fc654e88240e
yeongjinoh <oyj9109@snu.ac.kr>
2016-10-19 18:59:09 +0900

    execute code loaded from database in fullscreen viewer

--- routes/jsloader.js
+++ routes/jsloader.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-router.get('/:dep', function (req, res) {
+router.get('/module/:dep', function (req, res) {
     db.one("SELECT ctext FROM code_store WHERE mstatus=1 AND title=${dep}", req.params)
         .then(function (data) {
             var code = data.ctext;
@@ -17,6 +17,26 @@ router.get('/:dep', function (req, res) {
             });
         });
 });
+
+router.get('/code/:cid', function (req, res) {
+    db.one("SELECT ctext, deps FROM code_store WHERE cid=${cid}", req.params)
+        .then(function (data) {
+            console.log(data);
+            // scope의 충돌을 막기 위해 읽어온 코드를 function으로 한번 감싸서 수행해줍니다.
+            res.json({
+                ctext: '(function(){' + data.ctext + '\n})();',
+                deps: data.deps
+            });
+        })
+        .catch(function (error) {
+            console.log("ERROR:", error.message || error);
+            res.json({
+                resultCode: -1,
+                msg: "코드를 불러올 수 없습니다."
+            });
+        });
+});
+
 
 module.exports = router;
 
