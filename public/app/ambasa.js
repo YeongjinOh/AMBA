953fad487e45f1e552cfaea009e44b949b5d5c45
yeongjinoh <oyj9109@snu.ac.kr>
2016-10-28 21:37:36 +0900

    add, delete slide

--- public/app/ambasa.js
+++ public/app/ambasa.js
@@ -5,18 +5,36 @@
 
     /** basic setting for layout **/
 
-    var w = window.outerWidth, h = window.outerHeight;
+    // var w = window.outerWidth, h = window.outerHeight;
+    var w = 1280, h = 800;
     var menuBarWidth = w, menuBarHeight = 100, slideListWidth = 220, slideListHeight = h-menuBarHeight,
         slideEditorWidth = w-slideListWidth, slideEditorHeight = slideListHeight;
     var parent = div().append().size(w,h);
     var menuBar = div().appendTo(parent).size(menuBarWidth, menuBarHeight).borderBottom(borderGray);
     var slideList = div().appendTo(parent).size(slideListWidth, slideListHeight).borderRight(borderGray).color(slideListColor).overflowAuto();
     var slideEditor = div().appendTo(parent).size(slideEditorWidth, slideEditorHeight).color(slideEditorColor).overflowAuto();
+    var blank = div();
+    var refresh = function () {
+        blank.append();
+    };
 
     /** menu bar **/
 
-    var fileName = div().appendTo(menuBar).size(200,30).margin(20)
-        .text('제목 없는 프레젠테이션').fontColor('gray').fontSize(20);
+    var fileInfoHeader = div().appendTo(menuBar).size('100%',60);
+    var toolBar = div().appendTo(menuBar).size('100%',40);
+    var fileName = div().appendTo(fileInfoHeader).size(200,30).margin(20).text('제목 없는 프레젠테이션').fontColor('gray').fontSize(20);
+    var decoButton = function (div) {
+        div.size(25,25).marginLeft(20).cursorPointer();
+    };
+    var newSlideButton = div().appendTo(toolBar).deco(decoButton).image('../images/newslide.png').marginLeft(30)
+        .click(function () {
+            slideManager.new();
+        });
+    var delSlideButton = div().appendTo(toolBar).deco(decoButton).image('../images/delslide.png')
+        .click(function () {
+            slideManager.del();
+        });
+
 
     /** set slide background **/
 
@@ -32,5 +50,76 @@
     var slideBackground = div().appendTo(slideEditor).size(sbgWidth,sbgHeight).marginLeft(sbgMarginLeft).marginTop(sbgMarginTop)
         .color('white').border(borderGray).draggable().boxShadow('0px 5px 20px #888888')
         .resizable({aspectRatio:true});
+
+    /** Slide Manager, Slide **/
+
+    var Slide = function () {
+        var objs = {};
+
+    };
+
+    var SlideBlock = function () {
+        var blockWrapperHeight = 130, blockHeight = blockWrapperHeight*0.85, slideViewerWidth = blockHeight*w/h;
+        var blockWrapper = div().size('100%',blockWrapperHeight).paddingTop(blockWrapperHeight/10);
+        var block = div().appendTo(blockWrapper);
+        var numberViewer = div().appendTo(block).size(10,blockHeight).margin(5).text('1');
+        var slideViewer = div().appendTo(block).size(slideViewerWidth,blockHeight).color('white').border(borderGray);
+        var that = this;
+        var active = function () {
+            slideViewer.border('2px solid #BF360C');
+        };
+        this.deactive = function () {
+            slideViewer.border(borderGray);
+        };
+        this.setIdx = function (idx) {
+            numberViewer.text(idx);
+        };
+        this.getIdx = function () {
+            return parseInt(numberViewer.text());
+        };
+        this.appendTo = function (target) {
+            blockWrapper.appendTo(target);
+            return this;
+        };
+        this.remove = function () {
+            blockWrapper.remove();
+            refresh();
+        };
+        blockWrapper.click(function () {
+            if (curSlideBlock)
+                curSlideBlock.deactive();
+            curSlideBlock = that;
+            active();
+        });
+    };
+
+
+    var SlideManager = function () {
+        var slideBlocks = [];
+
+        this.new = function () {
+            // var slide = new Slide();
+            var slideBlock = new SlideBlock();
+            slideBlock.appendTo(slideList).setIdx(slideBlocks.push(slideBlock));
+        };
+
+        // curSlideBlock을 지운다.
+        this.del = function () {
+            if (curSlideBlock) {
+                var idx = curSlideBlock.getIdx()-1;
+                var slideBlock = slideBlocks.splice(idx,1)[0];
+                slideBlock.remove();
+                for (var i=idx; i<slideBlocks.length; i++) {
+                    slideBlocks[i].setIdx(i+1);
+                }
+                curSlideBlock = undefined;
+            }
+        };
+    };
+
+    /** Initialize **/
+
+    var slideManager = new SlideManager();
+    var curSlideBlock;
 
 })();
