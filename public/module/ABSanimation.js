ee8b4e5a4ea5a884f4286290c9fcdf9f5e1990d0
JiSoo <yjsgoon@naver.com>
2016-11-06 02:52:08 +0900

    for animation module test

--- public/module/ABSanimation.js
+++ public/module/ABSanimation.js
@@ -24,10 +24,12 @@ define ([], function() {
                 },
                 append: function () {
                     var i;
+                    // var effects = ['fadein', 'fadeout', 'slidedown', 'slideup'];
                     var params = ['duration', 'easing', 'complete'];
 
                     function editParams(dv, param) {
-                        var root = div().appendTo(dv).size('100%', 30).marginTop(15);
+                        var target = dv;
+                        var root = div().class('abs-option').appendTo(target).size('100%', 30).marginTop(15);
                         div().appendTo(root).size('30%', '100%').text(param + ': ').fontBold();
                         var edit = div().id('abs-ani-' + param).appendTo(root).size('40%', '75%').color('white').text('').editable().borderRadius(2)
                             .border('2px solid black').cursorText().textAlignLeft().keypress(function (dv, e) {
@@ -41,15 +43,18 @@ define ([], function() {
                                 e.preventDefault();
                             });
 
-                        var input = div().appendTo(root).size('20%', '75%').color('#cccccc').text('OK').fontBold()
-                            .textAlignCenter().cursorPointer().hoverColor('gray', '#cccccc').border('2px solid black').click(function (dv, e) {
+                        var input = div().appendTo(root).size('20%', '75%').color('#eeeeee').text('OK').fontBold()
+                            .textAlignCenter().cursorPointer().hoverColor('gray', '#eeeeee').border('2px solid black').click(function (dv, e) {
                                 e.stopPropagation();
                                 e.preventDefault();
 
                                 if (param === 'complete') {
                                     var aniData = {
                                         id: curId(), // Current ID를 반환, 없으면 undefined
-                                        params: getArgs()
+                                        animation: {
+                                            effect: target.id().split('-')[2],
+                                            params: getArgs()
+                                        }
                                     };
                                     animationQueue.push(aniData);
                                 } else {
@@ -70,7 +75,7 @@ define ([], function() {
                         }
                     }
 
-                    var root = div().appendTo(target).size('100%', '100%').border('2px solid black').selectable(false).overflowScroll();
+                    var root = div().class('abs-option').appendTo(target).size('100%', '100%').border('2px solid black').selectable(false).overflowScroll();
                     div().appendTo(root).size('100%', '6%').color('black').text('AMBASA - Animation').fontColor('white').fontSize(20).textAlignCenter().border('2px solid gray');
 
                     var decoContent = function (dv) {
@@ -105,7 +110,7 @@ define ([], function() {
                     });
 
 
-                    var ani_fadein = div().id('abs-ani-fadein').appendTo(fadeIn).displayNone().size('100%', 150).click(function (dv, e) {
+                    var ani_fadein = div().id('abs-ani-fadeIn').appendTo(fadeIn).displayNone().size('100%', 150).click(function (dv, e) {
                         e.stopPropagation();
                         e.preventDefault();
 
@@ -115,7 +120,7 @@ define ([], function() {
                     for (i = 0; i < params.length; i++)
                         editParams(ani_fadein, params[i]);
 
-                    var ani_fadeout = div().id('abs-ani-fadeout').appendTo(fadeOut).displayNone().size('100%', 150).click(function (dv, e) {
+                    var ani_fadeout = div().id('abs-ani-fadeOut').appendTo(fadeOut).displayNone().size('100%', 150).click(function (dv, e) {
                         e.stopPropagation();
                         e.preventDefault();
 
@@ -125,7 +130,7 @@ define ([], function() {
                     for (i = 0; i < params.length; i++)
                         editParams(ani_fadeout, params[i]);
 
-                    var ani_slidedown = div().id('abs-ani-slidedown').appendTo(slideDown).displayNone().size('100%', 150).click(function (dv, e) {
+                    var ani_slidedown = div().id('abs-ani-slideDown').appendTo(slideDown).displayNone().size('100%', 150).click(function (dv, e) {
                         e.stopPropagation();
                         e.preventDefault();
 
@@ -135,7 +140,7 @@ define ([], function() {
                     for (i = 0; i < params.length; i++)
                         editParams(ani_slidedown, params[i]);
 
-                    var ani_slideup = div().id('abs-ani-slideup').appendTo(slideUp).displayNone().size('100%', 150).click(function (dv, e) {
+                    var ani_slideup = div().id('abs-ani-slideUp').appendTo(slideUp).displayNone().size('100%', 150).click(function (dv, e) {
                         e.stopPropagation();
                         e.preventDefault();
 
@@ -145,33 +150,51 @@ define ([], function() {
                     for (i = 0; i < params.length; i++)
                         editParams(ani_slideup, params[i]);
                 },
+                animator: function (index) {
+                    var ani_package = animationQueue[index];
+                    eval("$(#"+ani_package.id+").data('div')."+ani_package.effect+"("+ani_package.params.duration+");");
+                },
                 animationManager: function () {
-                    var iter = 0;
+                    var index = 0;
                     var manager = {
-                        next: function () {
-                            return animationQueue[iter++];
+                        next: function() {
+                            if (this.hasNext()) {
+                                this.animator(index++);
+                                //return animationQueue[index++];
+                            }
+                            return undefined;
                         },
-                        hasNext: function () {
-
+                        hasNext: function() {
+                            if (index === animationQueue.length)
+                                return false;
+                            return true;
                         },
-                        back: function () {
-
+                        back: function() {
+                            if (this.hasBack()) {
+                                this.animator(--index);
+                                // return animationQueue[--index];
+                            }
+                            return undefined;
                         },
-                        hasBack: function () {
-
+                        hasBack: function() {
+                            if (index === 0)
+                                return false;
+                            return true;
                         },
-                        react: function () {
-
+                        goFirst: function() {
+                            index = 0;
+                            return index;
                         },
-                        lastact: function () {
+                        goLast: function() {
+                            index = animationQueue.length - 1;
+                            return index;
+                        },
+                        pop: function() {
 
                         }
                     };
 
                     return manager;
-                },
-                animator: function (selector) {
-
                 }
             };
 
