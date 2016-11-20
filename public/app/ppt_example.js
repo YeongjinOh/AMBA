6daf07ca74eb486c3a0fc53b3586c08ff13394da
JiSooYoon <jisooyoon@yunjisuui-MacBook-Air.local>
2016-11-20 20:46:59 +0900

    add ppt test

---
+++ public/app/ppt_example.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jisooyoon on 2016. 11. 20..
+ */
+
+div();
+
+var root = div().append();
+
+root.size('50%', 300).color('red');
+
+var main = div().appendTo(root).text('AMBA Project').fontSize(30).fontBold();
